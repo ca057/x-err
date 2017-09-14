78a33324f8e5d2e0e4843b42f9d6b0096c41e727
Christian Ost <christian@christianost.de>
2017-09-14 21:46:18 +0200

    inital commit of code

---
+++ index.js
@@ -0,0 +1,37 @@
+// see https://stackoverflow.com/questions/1382107/whats-a-good-way-to-extend-error-in-javascript
+
+function XError(props) {
+  this.name = 'XError';
+  this.message = '';
+
+  if (props && Object.keys(props).length > 0) {
+    Object.keys(props).forEach(key => {
+      Object.defineProperty(this, key, {
+        // TODO Test
+        enumerable: true,
+        // TODO test
+        writable: false,
+        value: props[key],
+      });
+    });
+  }
+
+  if (props && typeof props === 'string') {
+    this.message = props;
+  }
+
+  if (Error.hasOwnProperty('captureStackTrace')) {
+    Error.captureStackTrace(this, XError);
+  } else {
+    Object.defineProperty(this, 'stack', {
+      enumerable: false,
+      writable: false,
+      value: new Error(this.message).stack,
+    });
+  }
+}
+
+XError.prototype = Object.create(Error.prototype);
+XError.prototype.constructor = XError;
+
+module.exports = XError;
---
+++ index.test.js
@@ -0,0 +1,84 @@
+#! /usr/bin/env node
+const assert = require('assert');
+const XError = require('.');
+
+const printEmptyLine = () => console.log('');
+
+let passed = 0;
+let failed = 0;
+function test(doc, assertion) {
+  try {
+    assertion.call(null);
+    console.log('\x1b[32m%s\x1b[0m', 'PASS:', doc);
+    passed++;
+  } catch (ignoreMe) {
+    console.warn('\x1b[31m%s\x1b[0m', 'FAIL:', doc);
+    failed++;
+  }
+}
+
+printEmptyLine();
+
+test('(typeof) should return the same when checking its type as the native Error', () => {
+  const typeOfError = typeof Error;
+  const typeOfXError = typeof XError;
+  assert.equal(typeOfXError, typeOfError);
+});
+
+test('(instanceof) error object should be instanceof XError', () => {
+  const xError = new XError();
+  assert.ok(xError instanceof XError);
+});
+
+test('should have a message property if string is passed on construction', () => {
+  const testMessage = 'testMessage';
+  const xError = new XError(testMessage);
+  assert.ok(xError.message === testMessage);
+});
+
+test('should have an empty string as default message', () => {
+  assert.equal(new XError().message, '');
+});
+
+test('should be named XError', () => {
+  assert.equal(new XError().name, 'XError');
+});
+
+test('should return the correct error stack', () => {
+  function testFunction(message) {
+    try {
+      throw new XError(message);
+    } catch (error) {
+      return error.stack;
+    }
+  }
+  const errorMessage = 'errorStack';
+  const errorStack = testFunction(errorMessage).split('\n');
+  console.log(errorStack);
+  // should report an error with the provided message
+  assert.ok(errorStack[0].includes(errorMessage));
+  // error was thrown in testFunction()
+  assert.ok(errorStack[1].includes('testFunction'));
+  // testFunction() was called in test
+  assert.ok(errorStack[2].includes('test'));
+});
+
+test('should have all key/value-pairs of a passed object as property with the respective value', () => {
+  const testObject = {
+    foo: 'foo',
+    bar: 'bar',
+  };
+  const xError = new XError(testObject);
+  assert.equal(xError.foo, testObject.foo);
+  assert.equal(xError.bar, testObject.bar);
+});
+
+printEmptyLine();
+
+const result = `--> ${passed +
+  failed} tests completed (${passed} passed, ${failed} failed)`;
+if (failed > 0) {
+  console.log('\x1b[41m\x1b[37m%s\x1b[0m', 'FAILED', result);
+} else if (passed > 0 && failed === 0) {
+  console.log('\x1b[42m%s\x1b[0m', 'PASSED', result);
+}
